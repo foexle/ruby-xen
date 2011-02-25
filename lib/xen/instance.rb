f1af1cd99c3578208bb7b60ef51a8eff3a6fc8d1
Clemens Kofler <clemens@railway.at>
2011-02-25 16:31:29 +0100

    Instance.create

--- lib/xen/instance.rb
+++ lib/xen/instance.rb
@@ -2,11 +2,6 @@ module Xen
   class Instance
     attr_accessor :name, :memory, :dom_id, :vcpus, :state, :time
 
-    def initialize(name, options = {})
-      @name = name
-      @memory, @dom_id, @vcpus, @state, @time = options.values_at(:memory, :dom_id, :vcpus, :state, :time)
-    end
-
     class << self
       def all
         output = System::Command.exec_command("xm list" , :command_level => 1)
@@ -25,6 +20,22 @@ module Xen
         return domu
       end
 
+      def create(attributes = {})
+        Log::LibLoghandler.log(:log_level => LOG_LEVEL).info("Creating new Xen instance with name: #{attributes[:name]} ...")
+
+        password = Xen::Util.generate_root_password
+
+        if password[:exitstatus] == 0
+          command = <<-cmd
+            xen-create-image --hostname=#{attributes[:name]} --password=#{attributes[:stdout]}
+                             --vcpus=#{attributes[:cpus]} --memory=#{attributes[:memory]} --size=#{attributes[:hdd]}
+                             --arch=amd64 --dist=lucid
+          cmd
+
+          System::Command.exec_command(command, :command_level => 2)
+        end
+      end
+
       def instances_from_output(output)
         output[:stdout].split("\n")[1..-1].map do |line|
           instance_from_output(line)
@@ -38,6 +49,31 @@ module Xen
 
         Instance.new($1.strip, :dom_id => $2.strip, :memory => $3.strip, :vcpus => $4.strip, :state => $5.strip.gsub("-","")) #:time => $6.strip)
       end
+    end
+
+    def initialize(name, options = {})
+      @name = name
+      @memory, @dom_id, @vcpus, @state, @time = options.values_at(:memory, :dom_id, :vcpus, :state, :time)
+    end
+
+    def start
+      System::Command.exec_command("xm create #{name}.cfg", :command_level => 2)
+    end
+
+    def destroy
+      System::Command.exec_command("xm destroy #{dom_id}", :command_level => 1)
+    end
+
+    def pause
+      System::Command.exec_command("xm pause #{dom_id}", :command_level => 1) unless paused?
+    end
+
+    def unpause
+      System::Command.exec_command("xm unpause #{dom_id}", :command_level => 1) if paused?
+    end
+
+    def paused?
+      state == 'p'
     end
   end
 end
