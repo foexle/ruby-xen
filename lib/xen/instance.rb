2afebe3098835639f7012c6d7f4309a1ab021c06
Clemens Kofler <clemens@railway.at>
2011-02-25 16:05:36 +0100

    tiny

--- lib/xen/instance.rb
+++ lib/xen/instance.rb
@@ -2,14 +2,9 @@ module Xen
   class Instance
     attr_accessor :name, :memory, :dom_id, :vcpus, :state, :time
 
-    def initialize(name, options=())
-      @name=name
-      @memory=options[:memory]
-      @dom_id=options[:dom_id]
-      @vcpus=options[:vcpus]
-      @state=options[:state]
-      @time=options[:time]
+    def initialize(name, options = {})
+      @name = name
+      @memory, @dom_id, @vcpus, @state, @time = options.values_at(:memory, :dom_id, :vcpus, :state, :time)
     end
-
   end
 end
