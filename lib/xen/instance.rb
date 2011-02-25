c68ebaea9e022081e1e8b0ebe683d5e72bbb10ea
Clemens Kofler <clemens@railway.at>
2011-02-25 16:23:12 +0100

    implement Instance.all

--- lib/xen/instance.rb
+++ lib/xen/instance.rb
@@ -8,6 +8,13 @@ module Xen
     end
 
     class << self
+      def all
+        output = System::Command.exec_command("xm list" , :command_level => 1)
+        return [] unless output[:exitstatus] == 0
+
+        instances_from_output(output[:stdout])
+      end
+
       def find_by_name(name)
         domu = System::Command.exec_command("xm list #{name}", :command_level => 1)
 
@@ -17,14 +24,20 @@ module Xen
 
         return domu
       end
-    end
 
-    protected
+      def instances_from_output(output)
+        output[:stdout].split("\n")[1..-1].map do |line|
+          instance_from_output(line)
+        end
+      end
 
       def instance_from_output(output)
+        Log::LibLoghandler.log({:log_level => LOG_LEVEL}).debug("Finding #{output} ...")
+
         return unless output.match(/(.*)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+(\d+.\d)/)
 
         Instance.new($1.strip, :dom_id => $2.strip, :memory => $3.strip, :vcpus => $4.strip, :state => $5.strip.gsub("-","")) #:time => $6.strip)
       end
+    end
   end
 end
